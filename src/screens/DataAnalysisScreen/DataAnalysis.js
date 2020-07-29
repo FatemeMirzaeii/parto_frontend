d1fef53266f8d85f5d66eb17199dfe8f874a2b6d
alirezakaram <alirezakaram.k74@gmail.com>
2020-07-29 11:16:36 +0430

    a

---
+++ src/screens/DataAnalysisScreen/DataAnalysis.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DataAnalysis = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default DataAnalysis;
