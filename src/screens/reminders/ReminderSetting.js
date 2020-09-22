b3539c1910d16af93a837605793a3e67375c3a42
MaryVosough <mvosooghian1992@gmail.com>
2020-09-22 13:11:13 +0330

    some changes in remindersetting

--- src/screens/reminders/ReminderSetting.js
+++ src/screens/reminders/ReminderSetting.js
@@ -23,7 +23,7 @@ const ReminderSetting = ({ navigation, route }) => {
   const [daysAgo, setDaysAgo] = useState(2);
   const [isLoading, setIsLoading] = useState(true);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     navigation.setOptions({
       title: reminder.title,
       headerLeft: () => (
@@ -51,126 +51,131 @@ const ReminderSetting = ({ navigation, route }) => {
         `INSERT INTO ${USER_REMINDER} (reminder_id, user_id, active, custom_message , custom_time, Xdays_ago) VALUES 
         (${reminder.id}, ${1}, ${
           isActive ? 1 : 0
-        }, '${message}', '${time}', ${daysAgo}) ON CONFLICT (user_id, reminder_id) DO UPDATE SET active=${
+        }, '${message}', '${hours}:${minutes}', ${daysAgo}) ON CONFLICT (user_id, reminder_id) DO UPDATE SET active=${
           isActive ? 1 : 0
-        }, custom_message='${message}', custom_time='${time}', Xdays_ago=${daysAgo}`,
+        }, custom_message='${message}', custom_time='${hours}:${minutes}', Xdays_ago=${daysAgo}`,
         [],
         USER_REMINDER,
       ).then(() => navigation.pop());
     };
-  }, [
-    isActive,
-    message,
-    time,
-    daysAgo,
-    navigation,
-    reminder.id,
-    reminder.title,
-  ]);
+  }, [isActive, message, hours, minutes, daysAgo, navigation, reminder.id]);
 
-  useEffect(() => {
-    db.rawQuery(
-      `SELECT * FROM ${USER_REMINDER} WHERE reminder_id=${reminder.id}`,
-      [],
-      USER_REMINDER,
-    ).then((res) => {
-      if (res.rows !== 'EMPTY_TABLE') {
-        console.log('dddddetail', res);
-        setData(res);
-        //if(res.active ==1) setIsActive(true); else setIsActive(false);
-        setIsActive(res.active === 1 ? true : false);
-        //setIsActive(res.active );
-        setMessage(res.custom_message);
-        setTime(res.custom_time);
-        setDaysAgo(res.Xdays_ago);
-        setIsLoading(false);
-      }
-      console.log('active', isActive);
-      console.log('mes', message);
-    });
-  }, [reminder.id]);
+  useLayoutEffect(
+    () => {
+      db.rawQuery(
+        `SELECT * FROM ${USER_REMINDER} WHERE reminder_id=${reminder.id}`,
+        [],
+        USER_REMINDER,
+      ).then((res) => {
+        if (res.rows !== 'EMPTY_TABLE') {
+          //   const t=res.custom_time.split(':');
+          // console.log('t', t);
+          console.log('dddddetail', res);
+          setData(res);
+          //if(res.active ==1) setIsActive(true); else setIsActive(false);
+          setIsActive(res.active === 1 ? true : false);
+          //setIsActive(res.active );
+          setMessage(res.custom_message);
+          //  setHours(t[0]);
+          //  setMinutes(t[1]);
+          setTime(res.custom_time);
+          setDaysAgo(res.Xdays_ago);
+          setIsLoading(false);
+        }
+        console.log('active', isActive);
+        console.log('mes', message);
+        // console.log('hours', hours);
+        // console.log('min', minutes);
+      });
+    },
+    //[ isActive,reminder.id,]
+    [reminder.id],
+  );
   console.log('data', data);
   console.log('dataactive', data.active);
   return (
     <ScrollView>
-      <Card>
-        <ListItem
-          title="یادآور"
-          leftIcon={
-            isActive ? (
-              <Icon
-                type="MaterialIcons"
-                name="notifications-active"
-                style={{ color: COLOR.btn }}
-              />
-            ) : (
-              <Icon
-                type="MaterialIcons"
-                name="notifications-off"
-                style={{ color: COLOR.textColorDark }}
-              />
-            )
-          }
-          switch={{
-            value: isActive,
-            onValueChange: setIsActive,
-            trackColor: { true: COLOR.lightPink, false: '#aaa' },
-            thumbColor: isActive ? COLOR.btn : '#f4f3f4',
-          }}
-          bottomDivider={isActive ? true : false}
-          titleStyle={styles.listItemTitle}
-          containerStyle={styles.listItem}
-          contentContainerStyle={styles.listItemContent}
-        />
-        {isActive ? (
-          <>
-            <PickerListItem
-              title="عنوان"
-              bottomDivider
-              leftIcon={
+      {data && (
+        <Card>
+          <ListItem
+            title="یادآور"
+            leftIcon={
+              isActive ? (
                 <Icon
-                  type="Entypo"
-                  name="new-message"
-                  style={{ color: '#aaa' }}
+                  type="MaterialIcons"
+                  name="notifications-active"
+                  style={{ color: COLOR.btn }}
                 />
-              }
-              customComponent={
-                <TextInput
-                  multiline
-                  style={styles.inputMessage}
-                  value={message}
-                  onChangeText={setMessage}
+              ) : (
+                <Icon
+                  type="MaterialIcons"
+                  name="notifications-off"
+                  style={{ color: COLOR.textColorDark }}
                 />
-              }
-              subtitle={message}
-            />
-            {
+              )
+            }
+            switch={{
+              value: isActive,
+              onValueChange: setIsActive,
+              trackColor: { true: COLOR.lightPink, false: '#aaa' },
+              thumbColor: isActive ? COLOR.btn : '#f4f3f4',
+            }}
+            bottomDivider={isActive ? true : false}
+            titleStyle={styles.listItemTitle}
+            containerStyle={styles.listItem}
+            contentContainerStyle={styles.listItemContent}
+          />
+          {isActive ? (
+            <>
               <PickerListItem
-                title="زمان"
-                subtitle={`${daysAgo} روز قبل ساعت ${hours}:${minutes}`}
+                title="عنوان"
+                bottomDivider
                 leftIcon={
                   <Icon
-                    type="FontAwesome"
-                    name="calendar-check-o"
+                    type="Entypo"
+                    name="new-message"
                     style={{ color: '#aaa' }}
-                    bottomDivider={false}
                   />
                 }
                 customComponent={
-                  <DateTimePicker
-                    daySelectedItem={daysAgo}
-                    onDaySelected={setDaysAgo}
-                    hourSelectedItem={Number(hours)}
-                    onHourSelected={setHours}
-                    minSelectedItem={Number(minutes)}
-                    onMinSelected={setMinutes}
+                  <TextInput
+                    multiline
+                    selectionColor={COLOR.btn}
+                    style={styles.inputMessage}
+                    value={message}
+                    onChangeText={setMessage}
                   />
                 }
+                subtitle={message}
               />
-            }
-          </>
-        ) : null}
-      </Card>
+              {
+                <PickerListItem
+                  title="زمان"
+                  subtitle={`${daysAgo} روز قبل ساعت ${hours}:${minutes}`}
+                  leftIcon={
+                    <Icon
+                      type="FontAwesome"
+                      name="calendar-check-o"
+                      style={{ color: '#aaa' }}
+                      bottomDivider={false}
+                    />
+                  }
+                  customComponent={
+                    <DateTimePicker
+                      daySelectedItem={daysAgo}
+                      onDaySelected={setDaysAgo}
+                      hourSelectedItem={Number(hours)}
+                      onHourSelected={setHours}
+                      minSelectedItem={Number(minutes)}
+                      onMinSelected={setMinutes}
+                    />
+                  }
+                />
+              }
+            </>
+          ) : null}
+        </Card>
+      )}
     </ScrollView>
   );
 };
