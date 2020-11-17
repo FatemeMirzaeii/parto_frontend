c464aa7b1348adafe1b988d971684046254b1f8a
MaryVosough <mvosooghian1992@gmail.com>
2020-11-17 12:27:33 +0330

    Add lottie in signUp

--- src/screens/signup/index.js
+++ src/screens/signup/index.js
@@ -1,22 +1,97 @@
-import React from 'react';
-import { KeyboardAvoidingView } from 'react-native';
+import React, {
+  useRef,
+  useState,
+  Fragment,
+  useContext,
+  useEffect,
+} from 'react';
+import axios from 'axios';
+import {
+  ToastAndroid,
+  View,
+  Text,
+  ActivityIndicator,
+  SafeAreaView,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+} from 'react-native';
+import { Icon, Input } from 'react-native-elements';
+import LottieView from 'lottie-react-native';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import Ptxt from '../../components/Ptxt';
+import DataBase from '../../util/database';
 import styles from './styles';
-import SignUpForm from './SignUpForm';
-import { Icon } from 'react-native-elements';
+import { api } from '../../services/api';
+import { storeData } from '../../util/func';
+import { AuthContext } from '../../contexts';
+import { COLOR, HEIGHT } from '../../styles/static';
 
 const SignUp = ({ navigation }) => {
+  const restapi = new api();
+  const emailInput = useRef(null);
+  const passInput = useRef(null);
+  const [isLoading, setLoading] = useState(false);
+  const [securePassword, setSecurePassword] = useState(true);
+  const { signUp } = useContext(AuthContext);
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [serverCode, setServerCode] = useState(0);
+  const [inputCode, setInputCode] = useState(0);
+
+  useEffect(() => {
+    const getVerificationCode = () => {
+      axios({
+        method: 'post',
+        url: `https://api.parto.app/auth/verifyCode`,
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data:{
+          "phone": phoneNumber,
+        }
+      })
+        .then((res) => {
+          console.log('res', res);
+          setServerCode(res.data.code)
+        })
+        .catch((err) => {
+          console.error(err, err.response);
+        });
+    };
+
+    getVerificationCode();
+  }, []);
+
   return (
-    <KeyboardAvoidingView>
-      <Icon
-        name="close"
-        color="#f50"
-        containerStyle={styles.close}
-        size={30}
-        onPress={() => {
-          navigation.pop();
-        }}
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor: '#fff', paddingTop: 24 }}>
+      <View style={{ height: HEIGHT / 2.5 }}>
+        <LottieView
+          source={require('../../../assets/lotties/verification.json')}
+          autoPlay
+          // enableMergePathsAndroidForKitKatAndAbove
+        />
+      </View>
+      <Ptxt style={styles.title}>برای ورود شماره موبایلت رو وارد کن:</Ptxt>
+      <TextInput
+        style={styles.phoneInput}
+        underlineColorAndroid="transparent"
+        placeholder="۹- - - - - - - - -"
+        //keyboardType='phone-pad' 
+        textContentType='telephoneNumber' 
+        dataDetectorTypes='phoneNumber' 
+        keyboardType={"numeric"}
+        placeholderTextColor='#aaa'
+        // onFocus={() => this.setState({ mask: true })}
+        // onChangeText={this._handleInput}
+        value={phoneNumber}
+        //secureTextEntry={true}
+        //{(txt)=>this.setState({phoneNum:txt})}  
+        maxLength={10}
+        //ref={(c) => this.refinput = c}
       />
-      <SignUpForm />
     </KeyboardAvoidingView>
   );
 };
--- src/screens/signup/styles.js
+++ src/screens/signup/styles.js
@@ -1,15 +1,56 @@
 import { StyleSheet } from 'react-native';
+import { COLOR, FONT, SIZE } from '../../styles/static';
 
 export default StyleSheet.create({
+  // container: {
+  //   backgroundColor: '#ffffff',
+  //   elevation: 10,
+  //   justifyContent: 'center',
+  //   height: '80%',
+  //   width: '90%',
+  //   alignSelf: 'center',
+  //   borderRadius: 25,
+  //   top: '10%',
+  // },
+  // button: {
+  //   alignSelf: 'center',
+  //   bottom: -30,
+  //   position: 'absolute',
+  // },
+  // close: {
+  //   position: 'absolute',
+  //   top: 15,
+  //   left: 15,
+  // },
+  // error: {
+  //   fontSize: 12,
+  //   color: 'red',
+  //   alignSelf: 'center',
+  // },
   container: {
     backgroundColor: '#ffffff',
-    elevation: 10,
+    paddingTop:30,
+    // elevation: 10,
     justifyContent: 'center',
-    height: '80%',
-    width: '90%',
+    // height: '80%',
+    // width: '90%',
     alignSelf: 'center',
-    borderRadius: 25,
-    top: '10%',
+    // borderRadius: 25,
+    // top: '10%',
+  },
+  phoneInput: {
+    // marginTop:6,
+    // margin: 5,
+    // marginLeft: 40,
+    margin:10,
+    alignSelf: 'center',
+    padding: 5,
+    height: 50,
+    width:120,
+    borderColor: '#9FE6FD',
+    borderWidth: 1,
+    fontSize:15,
+    fontFamily:FONT.regular
   },
   button: {
     alignSelf: 'center',
@@ -21,9 +62,9 @@ export default StyleSheet.create({
     top: 15,
     left: 15,
   },
-  error: {
-    fontSize: 12,
-    color: 'red',
+  title: {
+    fontSize:SIZE[15],
+    color: COLOR.black,
     alignSelf: 'center',
   },
 });
