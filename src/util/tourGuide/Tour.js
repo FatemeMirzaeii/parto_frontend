3be774df05380a74f5f705ebdb52c275da0d403c
MaryVosough <mvosooghian1992@gmail.com>
2020-11-23 20:38:36 +0330

    Edit Tour

--- src/util/tourGuide/Tour.js
+++ src/util/tourGuide/Tour.js
@@ -3,16 +3,7 @@ import { DeviceEventEmitter } from 'react-native';
 import { getData, storeData } from '../func';
 import { AppTour, AppTourSequence } from 'react-native-app-tour';
 
-//to maryam: Tour is undefined.
-//using variables without defining them is common in typescript(.ts) but for now we are using .js files so we should define them first.
-//this line should be like one of these two:
-//1. const Tour =()=>{}
-//export default Tour;
-//2. export default () => {}.  no name is needed!
-
-//due to our files naming convention in this project its good to name this file as 'index.js'
-
-export default Tour = (appTourTargets, key, storeKey) => {
+ const Tour = (appTourTargets, key, storeKey) => {
   const [appTour, setAppTour] = useState(true);
 
   useEffect(() => {
@@ -57,3 +48,5 @@ export default Tour = (appTourTargets, key, storeKey) => {
     setAppTour(keyInStore);
   };
 };
+
+export default Tour;
