f2fa9fd341e00c00a38baf51bcc0b4b324de3f9d
MaryVosough <mvosooghian1992@gmail.com>
2020-12-08 19:19:21 +0330

    Add useFetcher in hooks

---
+++ src/util/hooks/useFetcher.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+//util
+import { getData } from '../../util/func';
+
+//services
+import { baseUrl, devUrl } from '../../services/urls';
+
+const useFetcher = ({ method, dev = false, url, data = null }) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (method === 'get' || method === 'GET')
+        await axios({
+          method: method,
+          url: dev ? `${devUrl}${url}` : `${baseUrl}${url}`,
+          credentials: 'include',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'x-auth-token': await getData('@token'),
+          },
+        });
+      else
+        await axios({
+          method: method,
+          url: dev ? `${devUrl}${url}` : `${baseUrl}${url}`,
+          credentials: 'include',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'x-auth-token': await getData('@token'),
+          },
+          data: data,
+        })
+          .then((res) => {
+            setResponse(res.data);
+          })
+          .catch((err) => {
+            setError(err);
+          })
+          .finally(() => {
+            setIsLoading(false);
+          });
+    };
+
+    fetchData();
+  }, [method, dev, url, data]);
+
+  return { response, error, isLoading };
+};
+
+export default useFetcher;
