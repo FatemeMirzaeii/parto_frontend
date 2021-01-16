2f0c9b3125cd0270c4508789a3c06c85e86a407c
MaryVosough <mvosooghian1992@gmail.com>
2021-01-16 23:34:22 +0330

    Edit trackingOptions Screen

--- src/screens/tracking-options/styles.js
+++ src/screens/tracking-options/styles.js
@@ -1,18 +1,48 @@
 import { StyleSheet } from 'react-native';
-import { WIDTH, HEIGHT, FONT, SIZE } from '../../styles/static';
+import { WIDTH, HEIGHT, FONT, SIZE, COLOR } from '../../styles/static';
 
 export default StyleSheet.create({
   container: {
     height: '100%',
     backgroundColor: 'white',
-    justifyContent: 'space-between',
-    paddingTop: 25,
+    //justifyContent: 'space-between',
+    //paddingTop: 25,
+    // flex:1,
+  },
+  sky: {
+    width: '100%',
+    height: '100%',
+    // justifyContent:'flex-end'
+    // flex:1,
+    //justifyContent: 'space-around',
+  },
+
+  sliderWrapper: {
+    backgroundColor: 'white',
+    height: '75%',
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    marginTop: 5,
   },
   sliderItem: {
-    flex: 1,
+    //flex: 1,
+    height: '100%',
     alignItems: 'center',
+    //
     padding: 10,
-    paddingBottom: 50,
+    //paddingBottom: 50,
+    //backgroundColor:'lightgreen'
+  },
+  carousel: {
+    flexDirection: 'row',
+    width: WIDTH / 1.6,
+    // width: WIDTH /2,
+    marginTop: 10,
+    height: HEIGHT / 4.5,
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'space-between',
+    //backgroundColor:"red"
   },
   detailPage: {
     height: 200,
@@ -20,47 +50,81 @@ export default StyleSheet.create({
     alignItems: 'center',
   },
   category: {
-    margin: '7%',
-    marginBottom: 13,
+    //margin: '5%',
+    marginBottom: '5%',
+    //marginBottom: 13,
     width: WIDTH / 4,
     height: HEIGHT / 6.3,
     justifyContent: 'center',
     alignItems: 'center',
+    //backgroundColor:'#aaa'
   },
   options: {
     justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
     flex: 1,
     flexDirection: 'row',
-    marginTop: '5%',
+    //marginTop: '5%',
+    // backgroundColor:'pink',
+    // margin:10,
+    width: '70%',
+    // height: HEIGHT /5,
   },
   option: {
+    width: WIDTH / 3,
+    height: HEIGHT / 6,
     flex: 1,
     margin: 5,
+    alignSelf: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
+    // backgroundColor:'yellow',
+    elevation: 3,
+    borderRadius: 25,
   },
   icon: {
     width: WIDTH / 4.2,
     height: HEIGHT / 7.6,
-    borderWidth: 6,
-    borderRadius: 25,
+
+    // elevation: 3,
+    //backgroundColor:'red'
   },
   descriptionTxt: {
     fontFamily: FONT.medium,
     fontSize: SIZE[16],
     textAlign: 'center',
-    margin: 5,
-    paddingVertical: 5,
+    // margin: 5,
+    // paddingVertical: 5,
+  },
+  titleBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // paddingHorizontal: 10,
+    alignItems: 'center',
+    //backgroundColor:'yellow',
+    alignSelf: 'center',
+    marginLeft: '30%',
   },
   title: {
     fontFamily: FONT.medium,
     fontSize: SIZE[15],
-    textAlign: 'center',
-    paddingBottom: '10%',
+    textAlign: 'right',
+    //paddingBottom: '10%',
+    paddingRight: '20%',
+    //backgroundColor:'green'
   },
   txt: {
-    fontFamily: FONT.regular,
+    fontFamily: FONT.light,
     fontSize: SIZE[14],
     textAlign: 'center',
+    color: COLOR.black,
+    flex: 1,
+    paddingHorizontal: 5,
+    //backgroundColor:'green',
+    //  alignItems:'center',
+    //  alignSelf:'center',
+    //  marginBottom:10
   },
 
   more: {
