1927763f42aed507236677b0e84d428e97ea090e
Fateme Mirzaei <fatememirzaie@live.com>
2021-03-10 12:36:11 +0330

    correct null error messages

--- src/services/api.js
+++ src/services/api.js
@@ -24,8 +24,13 @@ export default async ({ url, data = null, method = 'GET', dev = false }) => {
       return res;
     }
   } catch (err) {
-    console.error(err, '--------', err.response.data.message, url);
-    if (err.status === 500 || err.status === 502) return;
+    console.error(err.response, '--------', err.response.data.message, url);
+    if (
+      err.response.status === 500 ||
+      err.response.status === 502 ||
+      !err.response.data.message
+    )
+      return;
     else if (err.toString() === 'Error: Network Error')
       ToastAndroid.show('لطفا اتصال اینترنت رو چک کن.', ToastAndroid.LONG);
     else ToastAndroid.show(err.response.data.message, ToastAndroid.SHORT);
