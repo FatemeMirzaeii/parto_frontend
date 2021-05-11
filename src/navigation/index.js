e801a48896684c129f8bae7d917ae63e6e162ae0
fateme <mirzaei.f@parto.email>
2021-05-11 13:06:02 +0430

    auth reducer moved to redux persist

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,24 +1,20 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { DateContext } from '../contexts';
 import AuthStack from './AuthStack';
 import InterviewStack from './InterviewStack';
 import HomeStackScreen from './HomeStack';
 import moment from 'moment';
 import { FORMAT } from '../constants/cycle';
-import { getData } from '../util/func';
 
 const AppNavigator = () => {
-  // const authStates = useSelector((state) => state.auth);
-  const userToken = getData('@token');
-  const interviewToken = getData('@startPages');
-  // correct way to read tokens is reading them from redux store, but in order to its unperdictable behavior and late loading,
-  // we will read them here direct from storage.
+  const authStates = useSelector((state) => state.auth);
+
   return (
     <DateContext.Provider value={{ today: moment().format(FORMAT) }}>
-      {interviewToken && userToken ? (
+      {authStates.interviewToken && authStates.userToken ? (
         <HomeStackScreen />
-      ) : !userToken ? (
+      ) : !authStates.userToken ? (
         <AuthStack />
       ) : (
         <InterviewStack />
