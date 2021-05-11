38ea27520e86ecf88b1d6a042cdc12b0bd82338b
fateme <mirzaei.f@parto.email>
2021-05-11 12:32:32 +0430

    adding dialog box for logout

--- src/screens/menu/index.js
+++ src/screens/menu/index.js
@@ -38,6 +38,7 @@ const Menu = ({ navigation }) => {
   const template = useSelector((state) => state.user.template);
   const dispatch = useDispatch();
   const { isVisible, toggle } = useModal();
+  const { isVisible: signOutVisible, toggle: signOutToggle } = useModal();
 
   useEffect(() => {
     determineLockStatus();
@@ -287,7 +288,7 @@ const Menu = ({ navigation }) => {
               color: COLOR.icon,
               size: 10,
             }}
-            onPress={exit}
+            onPress={signOutToggle}
             titleStyle={styles.listItemText}
             containerStyle={styles.listItem}
             contentContainerStyle={styles.listItemContent}
@@ -306,6 +307,21 @@ const Menu = ({ navigation }) => {
           await resetDatabase();
           dispatch(fetchInitialCycleData());
           toggle();
+          setIsLoading(false);
+        }}
+        secondBtnPress={toggle}
+      />
+      <DialogBox
+        isVisible={signOutVisible}
+        isLoading={isLoading}
+        hide={signOutToggle}
+        icon={<Icon type="parto" name="exit" color="#aaa" size={50} />}
+        text="آیا می‌خواهید از حساب کاربری خود خارج شوید؟"
+        twoButtons
+        firstBtnPress={async () => {
+          setIsLoading(true);
+          await exit();
+          signOutToggle();
           setIsLoading(false);
         }}
         secondBtnPress={toggle}
