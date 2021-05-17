0d597e9d7ebeb00a0e434ebcd734862afad04d4c
fateme <mirzaei.f@parto.email>
2021-05-17 12:31:02 +0430

    signOutToggle bug

--- src/screens/menu/index.js
+++ src/screens/menu/index.js
@@ -337,7 +337,7 @@ const Menu = ({ navigation }) => {
           signOutToggle();
           setIsLoading(false);
         }}
-        secondBtnPress={toggle}
+        secondBtnPress={signOutToggle}
       />
     </ScrollView>
   );
