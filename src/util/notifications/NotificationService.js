ea2841b4b0b2ae9d308c2e208086a6485dd21c68
MVosough <mvosooghian1992@gmail.com>
2021-06-17 14:25:38 +0430

    Fix bug

--- src/util/notifications/NotificationService.js
+++ src/util/notifications/NotificationService.js
@@ -113,8 +113,8 @@ export default class NotificationService {
     //todo: set 'when' for showing time in banner
     // this.lastId++;
     PushNotification.localNotificationSchedule({
-      // date: date,
-      date: new Date(Date.now() + 30 * 1000), // in 30 secs
+       date: date,
+      // date: new Date(Date.now() + 30 * 1000), // in 30 secs
 
       /* Android Only Properties */
       channelId: soundName ? 'sound-channel-id' : 'default-channel-id',
