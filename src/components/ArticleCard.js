67eee1696349bd6dc12faa368b4b385eaab479f7
fateme nazari <nazarifatemeh306.fn@gmail.com>
2021-06-17 21:03:52 +0430

    share article added

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Icon } from 'react-native-elements';
-import Share from 'react-native-share';
+import { shareContent } from '../util/func';
+import { blogUrl } from '../services/urls';
+
 //styles
 import { FONT } from '../styles/static';
+import { COLOR } from '../styles/static';
 
 const ArticleCard = (props) => {
   return (
@@ -23,27 +26,19 @@ const ArticleCard = (props) => {
               }
             />
           </View>
-          <Icon name="rocket" size={30} type="AntDesign" />
         </TouchableOpacity>
+        <View style={styles.icon}>
+          <Icon
+            name="sharealt"
+            size={20}
+            type="antdesign"
+            onPress={() => shareContent(`${blogUrl}${props.link}`)}
+          />
+        </View>
       </View>
     </View>
   );
 };
-// const shareContent = async (url) => {
-//   const shareOptions = {
-//     title: 'Share file',
-//     url: url,
-//     failOnCancel: false,
-//   };
-
-//   try {
-//     const ShareResponse = await Share.open(shareOptions);
-//     shareTxt = JSON.strinygif(ShareResponse, null, 2);
-//   } catch (error) {
-//     console.log('Error =>', error);
-//     shareTxt = 'error: '.concat(getErrorString(error));
-//   }
-// };
 
 export default ArticleCard;
 
@@ -95,5 +90,9 @@ const styles = StyleSheet.create({
     height: 110,
     borderRadius: 15,
     borderColor: 'black',
+  },
+  icon: {
+    right: 160,
+    bottom: 10,
   },
 });
