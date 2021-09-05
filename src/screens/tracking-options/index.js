80d5f67b483bcedb87bd306b6116a1811ba91e25
fateme nazari <nazarifatemeh306.fn@gmail.com>
2021-09-05 16:13:02 +0430

    partner health tracking

--- src/screens/tracking-options/index.js
+++ src/screens/tracking-options/index.js
@@ -1,44 +1,43 @@
-import React, {
-  useEffect,
-  useState,
-  useCallback,
-  createRef,
-  useContext,
-  useRef,
-} from 'react';
+import React, { useState, useContext, useCallback, useEffect } from 'react';
 import {
-  TouchableOpacity,
-  SafeAreaView,
-  ScrollView,
   View,
   Text,
-  ToastAndroid,
   FlatList,
   ImageBackground,
+  SafeAreaView,
 } from 'react-native';
-import moment from 'moment';
-import Carousel from 'react-native-snap-carousel';
-import { Icon, Overlay, ButtonGroup, Input } from 'react-native-elements';
-import ActionSheet from 'react-native-actions-sheet';
 import { SvgCss } from 'react-native-svg';
+import moment from 'moment';
+import { Icon } from 'react-native-elements';
 
-//redux
-import { useDispatch, useSelector } from 'react-redux';
+//styles
+import { COLOR, WIDTH } from '../../styles/static';
+import styles from './styles';
 
-//store
-import { updatePerdictions, updatePeriodDays } from '../../store/actions/cycle';
+//database
+import { USER_TRACKING_OPTION } from '../../constants/database-tables';
 
 //components
 import WeekCalendar from '../../components/WeekCalendar';
 import CalendarPointer from '../../components/CalendarPointer';
-import Loader from '../../components/Loader';
-import HTMLRender from '../../components/HTMLRender';
+
+//assets
+import MainBg from '../../../assets/images/main/home.png';
+import TeenagerBg from '../../../assets/images/teenager/home.png';
+import PartnerBg from '../../../assets/images/partner/home.png';
+
+//redux
+import { useSelector } from 'react-redux';
+
+//util
+import { getTrackingOptionData } from '../../util/database/query';
+import { FORMAT } from '../../constants/cycle';
 
 //constants
+import { DateContext } from '../../contexts';
 import {
   BLEEDING,
   EXCERSICE,
-  SPOTTING,
   VAGINAL,
   PAIN,
   MOOD,
@@ -52,47 +51,17 @@ import {
   MORE_ABOUT_EXCERSICE,
   MORE_ABOUT_SEX,
 } from '../../constants/health-tracking-info';
-import { DateContext } from '../../contexts';
 
-//util
-import {
-  addTrackingOption,
-  deselectTrackingOption,
-  getTrackingOptionData,
-  replaceTrackingOption,
-} from '../../util/database/query';
-import CycleModule from '../../util/cycle';
-import { FORMAT } from '../../constants/cycle';
-import Tour from '../../util/tourGuide/Tour';
-import { calendarMarkedDatesObject } from '../../util/func';
-
-//assets
-import MainBg from '../../../assets/images/main/home.png';
-import TeenagerBg from '../../../assets/images/teenager/home.png';
-import PartnerBg from '../../../assets/images/partner/home.png';
-
-//styles
-import styles from './styles';
-import { COLOR, WIDTH } from '../../styles/static';
-
-const detailPageRef = createRef();
-
-const TrackingOptions = ({ route }) => {
+const PartenerTrackingOptions = ({ route }) => {
+  const [categories, setCategories] = useState([]);
+  console.log('categories', categories);
+  const template = useSelector((state) => state.user.template);
+  const [appTourTargets, setAppTourTargets] = useState([]);
   const { today } = useContext(DateContext);
   const [date, setDate] = useState(route.params ? route.params.day : today);
-  const [selectedIndex, setSelectedIndex] = useState(null);
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [detailPageId, setDetailPageId] = useState(null);
-  const [visible, setVisible] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [visible, setVisible] = useState(false);
   const [overlayText, setOverlayText] = useState('');
-  const [categories, setCategories] = useState([]);
-  const [appTourTargets, setAppTourTargets] = useState([]);
-  const cycle = useSelector((state) => state.cycle);
-  const template = useSelector((state) => state.user.template);
-  const dispatch = useDispatch();
-  const categoryCarousel = useRef(null);
-  const optionsCarousel = useRef(null);
 
   const getInitialData = useCallback(async () => {
     const td = await getTrackingOptionData(date);
@@ -108,167 +77,37 @@ const TrackingOptions = ({ route }) => {
     getInitialData();
   }, [getInitialData]);
 
-  Tour(appTourTargets, 'calendarPointer', 'TrackingOptionTour');
+  const selectedItems = categories.filter((item) => item.selected.length > 0);
+  console.log('selectedItems', selectedItems);
 
-  const _categoryCarouselRenderItem = ({ item }) => {
+  const _renderItem = ({ item, color }) => {
     return (
-      <View style={styles.sliderItem}>
-        <View style={styles.category}>
-          <SvgCss width="100%" height="100%" xml={item.icon} />
-        </View>
+      <View
+        style={[
+          styles.option1,
+          {
+            backgroundColor: item.selected.length > 0 ? color : COLOR.white,
+          },
+        ]}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Icon
+          raised
+          name="info"
+          type="parto"
+          size={15}
+          onPress={() => {
+            toggleOverlay(item.id);
+          }}
+        />
+        <Text style={styles.txt}>{item.title}</Text>
+        <SvgCss
+          width="75%"
+          height="75%"
+          xml={item.icon}
+          fill={item.selected.length > 0 ? COLOR.white : color}
+        />
       </View>
     );
-  };
-
-  const _optionsCarouseRenderItem = ({ item }) => {
-    return (
-      <View style={styles.sliderItem}>
-        <View style={styles.options}>{renderOptions(item, item.color)}</View>
-      </View>
-    );
-  };
-
-  const renderOptions = (category, color) => {
-    return (
-      <FlatList
-        data={category.options}
-        numColumns={2}
-        keyExtractor={(item, index) => index.toString()}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            key={item.id}
-            onPress={() => onOptionPress(category, item)}
-            style={[
-              styles.option,
-              {
-                backgroundColor: item.selected.length > 0 ? color : COLOR.white,
-              },
-            ]}>
-            <SvgCss
-              width="75%"
-              height="75%"
-              xml={item.icon}
-              fill={item.selected.length > 0 ? COLOR.white : color}
-            />
-            <Text style={styles.txt}>{item.title}</Text>
-          </TouchableOpacity>
-        )}
-      />
-    );
-  };
-
-  const renderDetailPage = () => {
-    switch (detailPageId) {
-      case BLEEDING:
-        return (
-          <View style={{ flexDirection: 'row' }}>
-            <Input
-              placeholder="ساعت شروع پریود"
-              containerStyle={styles.input}
-              leftIcon={
-                <Icon name="ios-alarm" size={24} color="gray" type="ionicon" />
-              }
-            />
-            <ButtonGroup
-              onPress={updateIndex}
-              selectedIndex={selectedIndex}
-              buttons={['روز', 'َشب']}
-              vertical
-              containerStyle={styles.buttonGroup}
-              selectedButtonStyle={{ backgroundColor: '#F1719D' }}
-            />
-          </View>
-        );
-      case EXCERSICE:
-        return (
-          <View>
-            <Input
-              placeholder="مدت زمان ورزش"
-              containerStyle={styles.input}
-              leftIcon={
-                <Icon name="ios-alarm" size={24} color="gray" type="ionicon" />
-              }
-            />
-          </View>
-        );
-      default:
-    }
-  };
-
-  const _categoryCarouselSnap = (index) => {
-    optionsCarousel.current.snapToItem(index);
-    setActiveIndex(index);
-  };
-
-  const _optionsCarouselSnap = (index) => {
-    categoryCarousel.current.snapToItem(index);
-    setActiveIndex(index);
-  };
-
-  const onOptionPress = async (category, option) => {
-    if (template === 'Partner') return;
-    const c = await CycleModule();
-    if (option.selected.length > 0) {
-      await deselectTrackingOption(option.id, date);
-      getInitialData();
-      if (category.id === BLEEDING) {
-        const {
-          [date]: {},
-          ...periodDays
-        } = cycle.periodDays;
-        dispatch(updatePeriodDays(periodDays));
-      }
-    } else {
-      if (category.hasMultipleChoice) {
-        //this will add new option to others
-        await addTrackingOption(option.id, date);
-        getInitialData();
-      } else {
-        //this will remove other selected options and add new option
-        const sameCategoryOptions = category.options.map((o) => {
-          return o.id;
-        });
-        await replaceTrackingOption(option.id, date, sameCategoryOptions);
-        getInitialData();
-      }
-      // setDetailPageId(category.id);
-      // if (
-      //   (category.id === BLEEDING && option.id !== SPOTTING) ||
-      //   category.id === EXCERSICE
-      // ) {
-      //   detailPageRef.current?.setModalVisible();
-      // }
-    }
-    if (category.id === BLEEDING) {
-      await c.determineLastPeriodDate();
-      if (!(date in cycle.periodDays)) {
-        dispatch(
-          updatePeriodDays({
-            ...cycle.periodDays,
-            [date]: calendarMarkedDatesObject(COLOR.bleeding, false),
-          }),
-        );
-      }
-      dispatch(updatePerdictions());
-    }
-  };
-  const onDayPress = (d) => {
-    console.log('dayyyyyyyyy', d);
-    setActiveIndex(0);
-    categoryCarousel.current.snapToItem(0);
-    if (moment(d).isSameOrBefore(today)) {
-      setDate(d);
-    } else {
-      ToastAndroid.show(
-        'امکان ثبت شرح‌حال برای روزهای آتی وجود ندارد.',
-        ToastAndroid.LONG,
-      );
-    }
-  };
-
-  const updateIndex = (i) => {
-    setSelectedIndex(i);
   };
 
   const toggleOverlay = (itemId) => {
@@ -301,14 +140,6 @@ const TrackingOptions = ({ route }) => {
         break;
     }
   };
-
-  const _handleOnNext = () => {
-    categoryCarousel.current.snapToNext();
-  };
-  const _handleOnPrevious = () => {
-    categoryCarousel.current.snapToPrev();
-  };
-
   return (
     <SafeAreaView style={styles.container}>
       <ImageBackground
@@ -334,94 +165,22 @@ const TrackingOptions = ({ route }) => {
           }}
           current={date}
           maxDate={moment().format(FORMAT)}
-          onDateChanged={onDayPress}
-          onDayPress={(d) => onDayPress(d.dateString)}
+          //onDateChanged={onDayPress}
+          //onDayPress={(d) => onDayPress(d.dateString)}
         />
 
-        <View style={styles.sliderWrapper}>
-          {isLoading ? (
-            <Loader />
-          ) : (
-            <View style={styles.carousel}>
-              <Icon
-                raised
-                name="chevron-circle-left"
-                type="font-awesome"
-                color="#aaa"
-                size={15}
-                onPress={_handleOnNext}
-              />
-              <Carousel
-                inverted
-                //loop
-                ref={categoryCarousel}
-                data={categories}
-                renderItem={_categoryCarouselRenderItem}
-                sliderWidth={WIDTH / 1.4}
-                itemWidth={WIDTH / 4}
-                inactiveSlideScale={0.7}
-                inactiveSlideOpacity={0.5}
-                onSnapToItem={(index) => _categoryCarouselSnap(index)}
-              />
-              <Icon
-                raised
-                name="chevron-circle-right"
-                type="font-awesome"
-                color="#aaa"
-                size={15}
-                onPress={_handleOnPrevious}
-              />
-            </View>
-          )}
-          {categories.length !== 0 && (
-            <View style={styles.titleBox}>
-              <Text style={styles.title}>{categories[activeIndex].title}</Text>
-              <Icon
-                raised
-                name="info"
-                type="parto"
-                size={15}
-                onPress={() => {
-                  toggleOverlay(categories[activeIndex].id);
-                }}
-              />
-            </View>
-          )}
-          <Carousel
-            inverted
-            //loop
-            ref={optionsCarousel}
+        <View style={styles.partnerFlatList}>
+          <FlatList
             data={categories}
-            renderItem={_optionsCarouseRenderItem}
-            sliderWidth={WIDTH}
-            itemWidth={WIDTH}
-            onSnapToItem={(index) => _optionsCarouselSnap(index)}
+            numColumns={1}
+            showsVerticalScrollIndicator={true}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={_renderItem}
           />
         </View>
-        <Overlay isVisible={visible}>
-          <>
-            <Icon
-              type="antdesign"
-              name="close"
-              color="red"
-              size={20}
-              onPress={toggleOverlay}
-              containerStyle={styles.icon}
-            />
-            <ScrollView style={styles.htmlWrapper}>
-              <HTMLRender html={overlayText} />
-            </ScrollView>
-          </>
-        </Overlay>
-        <ActionSheet
-          ref={detailPageRef}
-          gestureEnabled={true}
-          bounceOnOpen={true}>
-          <View style={styles.detailPage}>{renderDetailPage()}</View>
-        </ActionSheet>
       </ImageBackground>
     </SafeAreaView>
   );
 };
 
-export default TrackingOptions;
+export default PartenerTrackingOptions;
