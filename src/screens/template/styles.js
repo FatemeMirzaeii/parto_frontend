b88c95e33c055e733cb6bb2ee34f43efbd690ccf
fateme <mirzaei.f@parto.email>
2021-10-02 16:39:21 +0330

    fixing some styles and removing extra styles.

--- src/screens/template/styles.js
+++ src/screens/template/styles.js
@@ -4,8 +4,9 @@ import { WIDTH, COLOR, FONT, HEIGHT } from '../../styles/static';
 export default StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: HEIGHT / 4.5,
-    justifyContent: 'space-around',
+    marginTop: HEIGHT / 5.5,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   bg: {
     width: '100%',
@@ -27,9 +28,10 @@ export default StyleSheet.create({
     marginBottom: 20,
   },
   whiteBox: {
-    width: WIDTH / 1.03,
-    height: HEIGHT / 6.6,
-    margin: 8,
+    width: WIDTH / 1.08,
+    margin: 5,
+    backgroundColor: COLOR.white,
+    alignItems: 'center',
     elevation: 2.5,
     borderRadius: 74,
     flexDirection: 'row-reverse',
@@ -39,52 +41,23 @@ export default StyleSheet.create({
     height: 100,
     marginTop: 10,
     marginRight: 10,
-    //style for carousel
-    // width: '100%',
-    // height: '100%',
-    // resizeMode: 'center',
   },
   textContainer: {
     flexDirection: 'column',
     alignItems: 'flex-end',
     marginTop: 10,
-    //style for carousel
-    // flexDirection: 'row-reverse',
-    // justifyContent: 'space-between',
-    // alignItems: 'center',
-    // marginTop: 20,
-    // width: 200,
   },
-  // textbox: {
-  //   elevation: 1,
-  //   borderRadius: 30,
-  //   width: 130,
-  // },
 
   templatetTitle: {
-    fontFamily: FONT.medium,
+    fontFamily: FONT.bold,
     fontSize: 15,
-    color: '#111111',
     lineHeight: 40,
   },
   desc: {
     fontFamily: FONT.regular,
-    fontSize: 14,
+    fontSize: 13,
     color: COLOR.textColor,
-    lineHeight: 26,
-    //style for carousel
-    //textAlign: 'center',
-    //lineHeight: 30,
-    //padding: 15,
   },
-  // carousel: {
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-  // item: {
-  //   width: 150,
-  //   height: 180,
-  // },
 
   button: {
     flexDirection: 'row-reverse',
